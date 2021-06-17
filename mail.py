4093e90ebbed61e48c79a8a0945cea3fe6275211
unavai <80496825+unavai@users.noreply.github.com>
2021-06-17 15:53:35 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -6,25 +6,20 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import sys
 
-id = sys.arv[1]
-password = sys.arv[2]
-sendEmail = sys.arv[3]
-subject= sys.arv[4]
-text = sys.argv[5]
-addrs = [sys.argv[6]]
+def navermail(id,password,sendEmail,subject,text,addrss):
+  addrs = [addrss]
+  smtp = smtplib.SMTP('smtp.gmail.com',587)
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login(id,password)
 
-smtp = smtplib.SMTP('smtp.gmail.com',587)
-smtp.ehlo()
-smtp.starttls()
-smtp.login(id,password)
+  message = MIMEMultipart()
+  message.attach(MIMEText(text))
 
-message = MIMEMultipart()
-message.attach(MIMEText(text))
+  for addr in addrs:
+    message['From'] = sendEmail
+    message['TO'] = addr
+    message['Subject'] = subject
+    smtp.sendmail(sendEmail,addr,message.as_string())
 
-for addr in addrs:
-  message['From'] = sendEmail
-  message['TO'] = addr
-  message['Subject'] = subject
-  smtp.sendmail(sendEmail,addr,message.as_string())
-
-smtp.quit()
+  smtp.quit()
