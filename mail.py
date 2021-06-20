206e305471e3730a2e74213bd12bc782e9eacfdd
qowlgns211 <78298283+qowlgns211@users.noreply.github.com>
2021-06-20 20:49:16 +0900

    구글 메일 보내기

--- mail.py
+++ mail.py
@@ -1,43 +1,45 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #!/usr/bin/python3
 
-import smtplib, os
+import smtplib
+import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.message import EmailMessage
 
-def navermail(id,password,sendEmail,subject,text,addrss):
-	
-	addrs =[addrss]
-	smtp = smtplib.SMTP('smtp.naver.com',587)
-	smtp.ehlo()
-	smtp.starttls()
-	smtp.login(id,password)
 
-	message = MIMEMultipart()
-	message.attach(MIMEText(text))
+def navermail(id, password, sendEmail, subject, text, addrss):
 
-	for addr in addrs:
-		message['From'] = sendEmail
-		message['TO'] = addr
-		message['Subject'] = subject
-		smtp.sendmail(sendEmail,addr,message.as_string())
+    addrs = [addrss]
+    smtp = smtplib.SMTP('smtp.naver.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(id, password)
 
-	smtp.quit()
+    message = MIMEMultipart()
+    message.attach(MIMEText(text))
+
+    for addr in addrs:
+        message['From'] = sendEmail
+        message['TO'] = addr
+        message['Subject'] = subject
+        smtp.sendmail(sendEmail, addr, message.as_string())
+
+    smtp.quit()
 
 
-def googlemail(email_from,email_to,email_subject,email_content,email_password):
+def googlemail(email_from, email_to, email_subject, email_content, email_password):
 
-	msg = EmailMessage()
-	msg.set_content(email_content)
+    msg = EmailMessage()
+    msg.set_content(email_content)
 
-	msg['From'] = email_from
-	msg['To'] = email_to
-	msg['Subject'] = email_subject
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = email_subject
 
-	smtp = smtplib.SMTP('smtp.gmail.com',587)
-	smtp.starttls()
-	smtp.login(email_from,email_password)
-	smtp.send_message(msg)
-	
-	smtp.quit()
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(email_from, email_password)
+    smtp.send_message(msg)
+
+    smtp.quit()
