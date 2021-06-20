cb3cec04096645952418d4eba3319009140d9491
unavai <80496825+unavai@users.noreply.github.com>
2021-06-20 10:59:17 +0900

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,25 +1,43 @@
 #-*- coding: utf-8 -*-
 #!/usr/bin/python3
 
-import stmpib, os
+import smtplib, os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import sys
+from email.message import EmailMessage
 
 def navermail(id,password,sendEmail,subject,text,addrss):
-  addrs = [addrss]
-  smtp = smtplib.SMTP('smtp.gmail.com',587)
-  smtp.ehlo()
-  smtp.starttls()
-  smtp.login(id,password)
+	
+	addrs =[addrss]
+	smtp = smtplib.SMTP('smtp.naver.com',587)
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login(id,password)
 
-  message = MIMEMultipart()
-  message.attach(MIMEText(text))
+	message = MIMEMultipart()
+	message.attach(MIMEText(text))
 
-  for addr in addrs:
-    message['From'] = sendEmail
-    message['TO'] = addr
-    message['Subject'] = subject
-    smtp.sendmail(sendEmail,addr,message.as_string())
+	for addr in addrs:
+		message['From'] = sendEmail
+		message['TO'] = addr
+		message['Subject'] = subject
+		smtp.sendmail(sendEmail,addr,message.as_string())
 
-  smtp.quit()
+	smtp.quit()
+
+
+def googlemail(email_from,email_to,email_subject,email_content,email_password):
+
+	msg = EmailMessage()
+	msg.set_content(email_content)
+
+	msg['From'] = email_from
+	msg['To'] = email_to
+	msg['Subject'] = email_subject
+
+	smtp = smtplib.SMTP('smtp.gmail.com',587)
+	smtp.starttls()
+	smtp.login(email_from,email_password)
+	smtp.send_message(msg)
+	
+	smtp.quit()
